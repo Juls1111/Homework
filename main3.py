9e02c0adb04f5b73929724afa713c6e3e73143e8
Juls1111 <99805587+Juls1111@users.noreply.github.com>
2022-02-27 22:45:40 +0300

    Update main3.py

--- main3.py
+++ main3.py
@@ -1,7 +1,7 @@
 def reliability(password):
     if (len(password) >= 8 and \
             any(i.islower() for i in password) and \
-            any(i.upper() for i in password) and \
+            any(i.isupper() for i in password) and \
             any(i.isdigit() for i in password)):
         return True
     else:
